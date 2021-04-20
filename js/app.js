bd632aeb443254867284c592b601ce6f9d980f7f
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-04-20 14:04:26 +0200

    Added the effect on the top of the screen too

--- js/app.js
+++ js/app.js
@@ -120,7 +120,7 @@ export default class Sketch {
         vec2 newUV = vUv;
         // step return 0 or 1 depending which one is bigger
         // smoothstep applies a gradient to the step
-        float area = smoothstep(0.4, 0., vUv.y);
+        float area = smoothstep(0.4, 0., vUv.y) - smoothstep(0.6, 1., vUv.y);
         area = pow(area,4.);
 
         //newUV.x += (vUv.x - 0.5)*0.5*vUv.y;
